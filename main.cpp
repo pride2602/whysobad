30c1bcf71ec86e2652bcf97ef3d8a9e405ee3f4b
pride2602 <107101181+pride2602@users.noreply.github.com>
2023-06-10 21:19:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,53 +1,23 @@
-#include "Vegetable.h"
-#include "Fruit.h"
+#include <iostream>
+#include "message.h"
+#include "User.h"
+#include "Chat.h"
 
 int main()
 {
-    IFood* grocery[4];
+    Chat chat;
 
-    grocery[0] = new Apple(50, string("Green"));
-    grocery[1] = new Banana(50, 10);
-    grocery[2] = new Potato(10, 10);
-    grocery[3] = new Tomato(10, string("cherry"));
+    chat.start();
 
-    bool open = true;
-    while (open)
+    bool cont();
     {
-        cout << "Choose product : 1 - Apple, 2 - Banana, 3 - Potato, 4 - Tomato, 0 to quit" << endl;
-        int choice;
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            grocery[0]->Show();
-            break;
+        chat.showLoginMenu();
 
-        case 2:
-            grocery[1]->Show();
-            break;
-
-        case 3:
-            grocery[2]->Show();
-            break;
-
-        case 4:
-            grocery[3]->Show();
-            break;
-
-        case 0:
-            open = false;
-            break;
-
-        default:
-            cout << "Choose product from 1 to 4 or 0 to quit" << endl;
-            break;
+        while (chat.getCurrentUser()) {
+            chat.showUserMenu();
         }
-    }
 
-    delete grocery[0];
-    delete grocery[1];
-    delete grocery[2];
-    delete grocery[3];
+    }
 
     return 0;
 }
